f6714c671208db534e3c2e60c451ec8645ed960c
agustin963 <230365@utxicotepec.edu.mx>
2024-10-16 22:42:30 -0600

    Practica 06
    se  modifico el archivo ejercicio04.js

--- variable/ejercicio04.js
+++ variable/ejercicio04.js
@@ -112,5 +112,68 @@ console.log("La lista actual es")
 estudiantes.unshift("Abishai Flores")
 console.table(estudiantes)
 
+console.log("%c7.- Eliminar el elemto de un arreglo en la ultima  posicion (POP)   ", style_console)
+console.log("El arreglo tiene los siguientes elemtos")
+console.table(estudiantes)
+console.log("Para este caso eliminamos a AbriL Guzman  en la ultima posicion")
+estudiantes.pop();
+console.log("Despues de eliminar el elemto el arreglo queda de la siguiente manera:")
+console.table(estudiantes)
+
+console.log("%c8.- Eliminar el elemto de un arreglo en la PRIMERA  posicion (SHIFT)   ", style_console)
+console.log("El arreglo tiene los siguientes elemtos")
+console.table(estudiantes)
+console.log("Para este caso eliminamos a Abishai Flores en la ultima posicion")
+estudiantes.shift();
+console.log("Despues de eliminar el elemto el arreglo queda de la siguiente manera:")
+console.table(estudiantes)
+
+console.log("%c9.-Modificar un arreglo en un arreglo nuevo con posiciones definidas (SPLICIE) ", style_console)
+console.log("El arreglo original tiene los siguientes elemetos ")
+console.table(estudiantes)
+console.log("Dividir el arreglo en uno nuevo eliminado ciertos elemtos en base a su posicion")
+// Cuando la funcion splice recibe un solo parametro eliminara los elemtos de esta posicion en adelante
+
+estudiantes.splice(2)
+console.table(estudiantes)
+// Cuando la funcion splice recibe 2 parametros elimina todos lo elementos arreglos que no estenten el rango
+estudiantes.push("Zyanya Zacatenco");
+estudiantes.push("Tania Ibarra");
+estudiantes.push("Juvenal Viveros");
+estudiantes.push("Marcos Rios ");
+estudiantes.push("Jhonathan Baldemar");
+console.log("Se agregaron 5 nuevos estudiantes , por lo que el arrgelo es:")
+console.table(estudiantes)
+console.log("Ahora ya tenemos los elemtos suficientes para aplicar el metodo splice con dos parametros que seran 3,5")
+estudiantes.splice(3,5)
+console.log(" El resultado en:")
+console.table(estudiantes);
+
+console.log(" Ahora vamos a insetar a \"Antonio Ocpaco \" en los elemtos de la posicion 0 y 1  " )
+estudiantes.splice(1,0,"Antonio Ocpaco");
+console.log("Resultando en :")
+console.table(estudiantes);
+
+// TAMBIEN SPLICE SIRVE PARA REEMPLAZAR ELEMTOS POR OTROS
+console.log(" Ahora vamos a reemplazar  a \"Angel Rufino\" en los elemtos de la posicion por  \"Brandon Leon\"  " )
+estudiantes.splice(0,1,"Brandon Leon");
+console.log("Resultando en :")
+console.table(estudiantes);
+
+console.log("%c10.- Metodos para la manipulacion de Arreglos INMUTABLES", style_console)
+let signosZodiacales = ["Aries","Tauro","Geminis","Cancer","Leo","Virgo","Libar","Escorpio","Sagitario","Capricarnio","Acuario","Picis"]
+// Destructuracion de Arreglos
+ 
+let [signo1,,signo3,,,,signo7,,,,,,] = signosZodiacales
+console.log(`El primer signo zodiacal es: ${signo1}`)
+console.log(`El tercero signo zodiacal es: ${signo3}`)
+console.log(`El septimo signo zodiacal es: ${signo7}`)
+//console.log(`El primer signo zodiacal es: ${signo4}`)
+
+//Congelamos el arreglo volviendolo inmutable 
+Object.freeze(signosZodiacales)
+
+
+
 
 
